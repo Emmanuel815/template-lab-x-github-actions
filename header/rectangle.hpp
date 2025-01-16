b85120f76ac1a78dca9e3c858915f8e563ce1726
Emmanuel815 <emisancheznp01@gmail.com>
2025-01-15 22:52:00 -0800

    Add rectangle area program
    * Added Rectangle class files and main file that calculates the rectangle's area.

--- header/rectangle.hpp
+++ header/rectangle.hpp
@@ -2,17 +2,13 @@
 #define RECTANGLE_HPP
 
 class Rectangle {
-    private:
-        int width;
-        int height;
-    public:
-        Rectangle();
-        Rectangle(int w,int h);
-
-        void set_width(int w);
-        void set_height(int h);
-        int area();
-        int perimeter();
+	private: 
+	    int width;
+	    int height;
+	public:
+	    void set_width(int w);
+	    void set_height(int h);
+	    int area();
 };
 
-#endif // RECTANGLE_HPP
+#endif /* RECTANGLE_HPP */
